4ab459acb5686dbf9d1450f5d50b27cb3207acad
Varg718 <r.wasilewski86@gmail.com>
2024-01-07 11:44:57 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
     modified:   money_exchange.py

--- money_exchange.py
+++ money_exchange.py
@@ -2,4 +2,17 @@ import requests
 
 url = requests.get('http://api.nbp.pl/api/exchangerates/tables/A/')
 response = url.json()
-print(response[0]['rates'])
+
+# Wyświetlenie dostępnych walut
+available_currencies = [rate['code'] for rate in response[0]['rates']]
+print('Available currencies:', available_currencies)
+
+currency = input('Which currency are you interested in?: ')
+quantity = int(input('How much do you want to exchange?: '))
+
+for rate in response[0]['rates']:
+    if currency == rate['code']:
+        result = quantity * float(rate['mid'])
+        print(f'You will received {currency}: {result} zlote!')
+        break
+
