11e957288927375e5a4b2187167454ab5687dcf6
Varg718 <r.wasilewski86@gmail.com>
2024-01-08 15:01:02 +0100

    Set 'PLN' as default currency, leave to choose any available currency.

--- money_exchange.py
+++ money_exchange.py
@@ -7,12 +7,37 @@ response = url.json()
 available_currencies = [rate['code'] for rate in response[0]['rates']]
 print('Available currencies:', available_currencies)
 
-currency = input('Which currency are you interested in?: ')
-quantity = int(input('How much do you want to exchange?: '))
+# Wybór waluty do wymiany
+while True:
+    try:
+        currency_to = input('Which currency do you want to exchange to?: ')
+        if currency_to not in available_currencies or currency_to == 'PLN':
+            raise ValueError('Invalid currency. Please choose a valid foreign currency.')
+        
+        break
+    except ValueError as e:
+        print(f'Error: {e}')
 
+# Wybór ilości do wymiany
+while True:
+    try:
+        quantity = float(input('How much PLN do you want to exchange?: '))
+        if quantity <= 0:
+            raise ValueError('Invalid quantity. Please enter a positive number.')
+        
+        break
+    except ValueError as e:
+        print(f'Error: {e}')
+
+# Wyszukiwanie kursu wymiany
+rate_to = 0
 for rate in response[0]['rates']:
-    if currency == rate['code']:
-        result = quantity * float(rate['mid'])
-        print(f'You will received {currency}: {result} zlote!')
+    if currency_to == rate['code']:
+        rate_to = float(rate['mid'])
         break
 
+if rate_to == 0:
+    print(f'Error: Unable to find exchange rate for {currency_to}.')
+else:
+    result = quantity / rate_to
+    print(f'You will receive {currency_to}: {result:.2f}!')
